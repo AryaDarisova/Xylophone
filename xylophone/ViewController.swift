8fa247cec2c8f27b94d1f894b92c4947d803b5c4
AryaDarisova <arisova.darya@yandex.ru>
2023-08-01 00:16:06 +0400

    Add play sound function only for "C" melody

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        
+//        print(url)
 
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
